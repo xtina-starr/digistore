93aeb41d6548507cb6febe4eff0542a16a8112d3
Christina Thompson <c.thompsonms@gmail.com>
2014-03-23 15:50:02 -0700

    added popular tunes

--- app/scripts/routes/home.js
+++ app/scripts/routes/home.js
@@ -1,3 +1,5 @@
 App.HomeRoute = Ember.Route.extend({
-
+  model: function () {
+      return this.store.find("product", {popular: true})
+    }
 });
--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -155,10 +155,23 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 Ember.TEMPLATES["home"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, self=this;
+
+function program1(depth0,data) {
   
+  var buffer = '', stack1;
+  data.buffer.push("\n  <div class=\"col-md-4\"><div class=\"song\">");
+  stack1 = helpers._triageMustache.call(depth0, "image", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</div></div>\n  ");
+  return buffer;
+  }
 
-
-  data.buffer.push("<h1>Welcome to the homepage</h1>\n\n<h2>Most popular songs:</h2>\n\n<div class=\"row\">\n  <div class=\"col-md-4\"><div class=\"song\"></div></div>\n  <div class=\"col-md-4\"><div class=\"song\"></div></div>\n  <div class=\"col-md-4\"><div class=\"song\"></div></div>\n</div>");
+  data.buffer.push("<h1>Welcome to the homepage</h1>\n\n<h2>Most popular songs:</h2>\n\n<div class=\"row\">\n  ");
+  stack1 = helpers.each.call(depth0, {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[],types:[],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n  <!-- <div class=\"col-md-4\"><div class=\"song\"></div></div>\n  <div class=\"col-md-4\"><div class=\"song\"></div></div> -->\n</div>");
+  return buffer;
   
 });
 
