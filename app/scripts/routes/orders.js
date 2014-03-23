1eec6c218c1bf0a3a332e76ddcf6ee2fa0d0d8ee
Christina Thompson <c.thompsonms@gmail.com>
2014-03-22 18:37:59 -0700

    save order_id

--- app/scripts/routes/orders.js
+++ app/scripts/routes/orders.js
@@ -2,17 +2,17 @@ App.OrdersRoute = Ember.Route.extend({
   actions: {
     checkout: function(proxy) {
       var order = this.store.createRecord("order", proxy);
+      var self = this
       this.store.find('cart', localStorage.cartId).then(function (cart) {
         order.set('cart', cart)
-      })
-      var self = this
-      order.save().then(function (order) {
+        order.save().then(function (order) {
           self.transitionTo("confirmation", order)
         }, function (error) {
           order.deleteRecord();
           alert("Your order could not be processed.")
         }
       ) 
+      })
     }
   },
   model: function () {
